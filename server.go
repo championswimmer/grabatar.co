228894b6065c80d82bc4b6b90d980904ebbe6e81
Arnav Gupta <championswimmer@gmail.com>
2023-05-25 02:42:01 +0530

    v0 of grabatar.co
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/:email?", func(c *fiber.Ctx) error {
+		if c.Params("email") != "" {
+			email := c.Params("email")
+			emailMD5 := md5.Sum([]byte(email))
+			emailHash := hex.EncodeToString(emailMD5[:])
+			c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+			return c.SendString("<img style=\"height: 512px; width: 512px; background: grey;\" src=\"https://www.libravatar.org/avatar/" + emailHash + "?s=512\">")
+		}
+		return c.SendString("To use please open in this format https://grabatar.co/you@email.com where 'you@email.com' is your email id")
+	})
+
+	app.Listen(":3000")
+}
